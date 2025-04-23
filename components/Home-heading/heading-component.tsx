df9108a1def4668cc89625ff8bbaf7f6c3eba479
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-04-23 18:16:28 +0200

    Heading section responsive

--- components/Home-heading/heading-component.tsx
+++ components/Home-heading/heading-component.tsx
@@ -10,13 +10,13 @@ const list = [
 
 function HeadingComponent() {
    return (
-      <div className="lg:flex lg:items-center lg:gap-[5.6vw] mb-[100px]">
-         <p className="bg-grey max-w-[500px] px-[25px] pt-[15px] pb-[20px] rounded-2xl mb-[35px] lg:mb-[0]">👉 Your digital success is our main destination. For years, Desite Software helps companies across the globe build new digital products, improve existing, and innovate within their business domain.
+      <div className="lg:flex lg:items-center lg:gap-[5.6vw] mb-[max(15.4166666667vw,_100px)]">
+         <p className="bg-grey max-w-[500px] px-[25px] pt-[15px] pb-[20px] rounded-2xl mb-[25px] lg:mb-[0]">👉 Your digital success is our main destination. For years, Desite Software helps companies across the globe build new digital products, improve existing, and innovate within their business domain.
          </p>
          <div className="max-w-[540px]">
-            <ul className="flex flex-wrap gap-[22px]">
+            <ul className="flex flex-wrap space-y-[22px]">
                {list.map(({ title }) =>
-                  <li key={title} className="w-[45%]">{title}</li>)}
+                  <li key={title} className="w-[50%] lg:w-[33.333%]">{title}</li>)}
             </ul>
          </div>
       </div>
--- components/Home-heading/heading-title.tsx
+++ components/Home-heading/heading-title.tsx
@@ -3,9 +3,10 @@ import Image from 'next/image'
 
 function HeadingTitle() {
    return (
-      <div className='text-[45px] font-bold mt-[150px] mb-[70px] flex flex-wrap'>
-         <p className='w-full'>We Create</p>
-         <div className='w-[45px] h-[65px] relative mr-[12px] ml-[12vw]'>
+      <div className='text-[9.7vw] xl:text-[7vw] mt-[95px] mb-[70px] flex flex-wrap leading-none xl:mb-[max(16vw,_180px)] xl:mt-[max(20vw,_300px)] 
+      xl:flex-nowrap xl:text-center  xl:items-center xl:gap-6 font-bold w-full '>
+         <p className='w-full xl:whitespace-nowrap xl:w-fit'>We Create</p>
+         <div className='w-[0.8em] relative mr-[12px] ml-[12vw] xl:mx-[0.3em] xl:aspect-[3/4] xl:w-[1em]'>
             <Image src="/Fire.png" alt='fire' fill />
          </div>
          <p>Systems</p>
